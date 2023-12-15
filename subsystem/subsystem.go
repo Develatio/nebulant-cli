13dbe863305d77c3b4f9a7e4585a23b801a9dcea
Nache <nache.nache@gmail.com>
2023-12-15 13:54:41 +0100

    hetzner/hetznerCloud

--- subsystem/subsystem.go
+++ subsystem/subsystem.go
@@ -114,7 +114,7 @@ func PrepareCmd(cmd *NBLcommand) error {
 		cast.SBus.RegisterProviderInitFunc("aws", aws.New)
 		cast.SBus.RegisterProviderInitFunc("azure", azure.New)
 		cast.SBus.RegisterProviderInitFunc("generic", generic.New)
-		cast.SBus.RegisterProviderInitFunc("hetzner", hetzner.New)
+		cast.SBus.RegisterProviderInitFunc("hetznerCloud", hetzner.New)
 		cast.SBus.RegisterProviderInitFunc("cloudflare", cloudflare.New)
 		blueprint.ActionValidators["providerValidator"] = func(action *blueprint.Action) error {
 			if _, err := cast.SBus.GetProviderInitFunc(action.Provider); err != nil {
